cc2a6a4573a044125b08da93df3a5901c1c812a9
k_study <onulee@naver.com>
2020-12-01 17:50:48 +0900

    12월 01일 수업

--- k_java/src/day7/Ex1201_08.java
+++ k_java/src/day7/Ex1201_08.java
@@ -1,5 +1,7 @@
 package day7;
 
+import java.util.Scanner;
+
 public class Ex1201_08 {
 
 	public static void main(String[] args) {
@@ -10,6 +12,35 @@ public class Ex1201_08 {
 		// 입력한 숫자를 배열을 지정해서 저장
 		// 마지막에 배열을 출력해보세요.
 		// 정답출력하시오.
+		Scanner scan = new Scanner(System.in);
+		int[] arr = new int[10];
+		int r_num = (int)(Math.random()*100)+1;
+		int count=1;
+		while(true) {
+			if(count<=10) {
+				System.out.println("숫자를 입력하세요.>>");
+				int input = scan.nextInt();
+				arr[count-1]=input;
+				if(r_num>input) {
+					System.out.println("더 큰수를 입력하세요!");
+				}else if(r_num<input) {
+					System.out.println("더 작은수를 입력하세요!");
+				}else {
+					System.out.println("정답입니다.");
+				}
+				count++;
+			}else {
+				break;
+			}
+		}
+		
+		
+		
+		
+		
+		System.out.println("정답 숫자 : "+r_num);
+		
+		
 
 	}
 
