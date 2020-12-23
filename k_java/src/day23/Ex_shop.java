bd4a69faa91ab40f3ccb226763bd18645482bf77
k_study <onulee@naver.com>
2020-12-23 17:42:57 +0900

    12월 23일 수업

--- k_java/src/day23/Ex_shop.java
+++ k_java/src/day23/Ex_shop.java
@@ -4,10 +4,9 @@ import java.util.Scanner;
 
 public class Ex_shop {
 	static Scanner scan = new Scanner(System.in);
+	static Buyer b1;
 
 	public static void main(String[] args) {
-		Buyer b1 = new Buyer("홍길동", 1000, 0); // 고객1명 생성
-		// 무한반복
 		loop1: while (true) {
 			System.out.println("[ 온라인쇼핑몰 ]");
 			System.out.println("1.회원등록");
@@ -19,13 +18,21 @@ public class Ex_shop {
 			System.out.println("----------------------");
 			System.out.println("원하는 번호를 입력하세요.>>");
 			int choice = scan.nextInt();
+			scan.nextLine();
 
 			switch (choice) {
 
 			case 1:
+				System.out.println("이름을 입력하세요.>>");
+				String name = scan.nextLine();
+				System.out.println("보충할 금액을 입력하세요.>>");
+				int coin = scan.nextInt();
+				b1 = new Buyer(name, coin, 0); // 고객1명 생성
+				// 무한반복
 				// 나중에 로그인
 				break;
 			case 2:
+				
 				b1.buy(new Tv());
 				System.out.println("TV 구매하였습니다.");
 				break;
